12f921fa9602e45572471182e01f58a3622a8e92
David Silveira <daviewave@gmail.com>
2021-10-26 02:06:27 -0400

    updated script.js by adding some dependencies targeting html elements, adding data attributes to gather todays date and the weather api key, and added the list of functions i will need to make this work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
 //DEPENDECIES
+//to target/add the searched cities attributes
+var $currentWeatherAttributes = $("#outline-current-weather");
+//to target/add the forecast cards in the container one by one
+var $forecastCards = $("#forecast-cards-container");
 
 //DATA
-const APIKey = ac5cdfbb230d2506d7a3adace9626884;
+//api key to gain access to weather api when requesting below
+const APIKey = "ac5cdfbb230d2506d7a3adace9626884";
+//variables that store the current day
+var currentDate = moment().format("MMMM Do YYYY");
+var currentYear = moment().format("YYYY");
+var currentDay = moment().format("DD");
+var currentMonth = moment().format("MM");
+
+console.log(currentYear);
+console.log(currentDay);
+console.log(currentMonth);
+
+var a = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=APIKey";
+console.log(a);
 
 //FUNCTIONS
+function createCurrentWeatherdisplay() {}
 
 //HELPER FUNCTIONS
+function getCity() {}
+
+function getCityTemperature() {}
+
+function getCityHumidity() {}
+
+function getCityWindSpeed() {}
+
+function getCityUVindex() {}
+
+function getTodaysDate() {}
+
+function getIconBasedOnCurrentWeather() {}
+
+function getColorOfUVindex() {}
 
 //USER INTERACTIONS
