3360d2cec351c052e77863926676bbba9cf2a997
David Silveira <daviewave@gmail.com>
2021-11-15 08:38:16 -0500

    changed structure of html so that the containers will already be on the page and so i will just have to target and edit them + got the City searched and the current date to be displayed after a user searches a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ var searchsArr = [];
 const APIKey = "ac5cdfbb230d2506d7a3adace9626884";
 
 //variables that store the current day
-var currentDate = moment().format("MMMM Do YYYY");
 var currentYear = moment().format("YYYY");
 var currentDay = moment().format("DD");
 var currentMonth = moment().format("MM");
@@ -28,48 +27,29 @@ var searchedCities = [];
 //FUNCTIONS
 function createCurrentWeatherdisplay() {}
 
-function createcurrentWeatherHeader($curWeather) {
-  var $currentWeatherHeader = $("<div>");
-  $currentWeatherHeader.addClass("col-12");
+// function createcurrentWeatherHeader(
+//   city,
+//   date,
+//   weatherIcon,
+//   temperature,
+//   humidity,
+//   windSpeed,
+//   UVindex
+// ) {
+//   var $currentWeatherHeader = $("<div>");
+//   $currentWeatherHeader.addClass("col-12");
 
-  var $cityName = $("<h1>");
-  $cityName.attr("id", "city-name-header");
-  $cityName.text(getCity());
+//   var $currentWeatherIcon = $("<img>");
 
-  var $currentWeatherIcon = $("<img>");
+//   var $cityName = $("<h1>");
+//   $cityName.attr("id", "city-name-header");
+//   $cityName.text(city + " (" + currentDate + ") ");
 
-  $currentWeatherHeader.append($cityName);
-  $currentWeatherHeader.append($currentWeatherIcon);
+//   $currentWeatherHeader.append($cityName);
+//   $currentWeatherHeader.append($currentWeatherIcon);
 
-  $curWeather.append($currentWeatherHeader);
-}
-
-//HELPER FUNCTIONS
-//returns a string of the name of the last city searched
-function getCity() {
-  if (searchCities === null) {
-    return "NO CITY SEARCHED YET";
-  } else {
-    curCity = JSON.stringify(searchCities[0]);
-    curCity = curCity.replace(/"/g,"");
-    console.log(curCity);
-    return curCity;
-  }
-}
-
-function getCityTemperature() {}
-
-function getCityHumidity() {}
-
-function getCityWindSpeed() {}
-
-function getCityUVindex() {}
-
-function getTodaysDate() {}
-
-function getIconBasedOnCurrentWeather() {}
-
-function getColorOfUVindex() {}
+//   $curWeather.append($currentWeatherHeader);
+// }
 
 //USER INTERACTIONS
 //when user clicks save need to handle in a function
@@ -79,10 +59,86 @@ var getInput = document.getElementById("search-button");
 getInput.addEventListener("click", function (event) {
   event.preventDefault();
 
+  //gets city to search based on input passed in
   var $input = $("#input");
   searchsArr.unshift($input.val());
-  // console.log(searchsArr);
   localStorage.setItem("searchedCities", JSON.stringify(searchsArr));
+  let currentCity = getCity();
+
+  let curURL = getAPIurl(currentCity);
+  let curDate = getCurDate();
+  setCurrentCityAndDate(getCity(), getCurDate());
+
+
+  // createcurrentWeatherHeader(currentCity, $currentWeatherAttributes);
 });
 
-createcurrentWeatherHeader($currentWeatherAttributes);
+//returns a string of the name of the last city searched
+function getCity() {
+  if (searchCities === null) {
+    return "NO CITY SEARCHED YET";
+  } else {
+    curCity = JSON.stringify(searchCities[0]);
+    curCity = curCity.replace(/"/g, "");
+    console.log(curCity);
+    return curCity;
+  }
+}
+
+function getCurDate(){
+  return moment().format("MMMM Do YYYY");
+}
+
+function setCurrentCityAndDate(city, date) {
+  var $cityAndDate = $("#cityAndDate");
+  $cityAndDate.text(city +  "  (" + date + ")  ");
+}
+
+//so I can get the rest of the stuff I need
+function getAPIurl(city) {
+  //Reaches out to API with url and key
+  let queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  console.log(queryURL);
+
+  return queryURL;
+}
+
+function getIconBasedOnCurrentWeather(APIurl){
+  
+  axios.get(APIurl)
+    .then(function (response) {
+
+    });
+    return $currentWeatherIcon;
+}
+
+function getCityTemperature(APIurl) {}
+
+function getCityHumidity(APIurl) {}
+
+function getCityWindSpeed(APIurl) {}
+
+function getCityUVindex(APIurl) {}
+
+function getColorOfUVindex(APIurl) {}
+
+
+
+
+
+
+// function getCurCityWeather(city) {
+//   //uses created url string to call and get info
+//   axios.get(queryURL).then(function (response) {
+//     //1. get weather icon
+//     let weatherIcon = response.data.weather[0].icon;
+//     let curTemp = response.data.main.temp;
+//     console.log(weatherIcon);
+//     console.log(curTemp);
+//   });
+// }
+// getCurCityWeather(getCity());
